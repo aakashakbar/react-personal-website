09b3fe8053853999cc912e181928a14802154d5a
Aakash Akbar <aakashakbar2@gmail.com>
2022-11-01 10:38:45 +0500

    updated contact links

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import resume from '../../assets/resume.pdf'
+import myresume from '../../assets/myresume.pdf'
 
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={resume} download className="btn">Download CV</a>
+            <a href={myresume} download className="btn">Download CV</a>
             <a href='#contact' className="btn btn__secondary">Contact</a>
         </div>
     )
--- src/Components/header/Header-icons.jsx
+++ src/Components/header/Header-icons.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import {FaGithub} from 'react-icons/fa'
 import {SiFiverr} from 'react-icons/si'
 import {SiUpwork} from 'react-icons/si'
+import {BsLinkedin} from 'react-icons/bs'
 
 const Icons = () => {
     return (
         <div className='headerIcons'>
-            <a href='https://www.google.com' target="_blank" rel="noreferrer"><FaGithub /></a>
-            <a href='https://www.google.com' target="_blank" rel="noreferrer"><SiFiverr /></a>
-            <a href='https://www.google.com' target="_blank" rel="noreferrer"><SiUpwork /></a>
+            <a href='https://www.linkedin.com/in/aakashakbar/' target="_blank" rel="noreferrer"><BsLinkedin /></a>
+            <a href='https://fiverr.com/aakashakbar' target="_blank" rel="noreferrer"><SiFiverr /></a>
+            <a href='https://www.upwork.com/freelancers/~01cebf20b12b3ad6b2' target="_blank" rel="noreferrer"><SiUpwork /></a>
+            <a href='https://github.com/aakashakbar' target="_blank" rel="noreferrer"><FaGithub /></a>
 
         </div>
     )
